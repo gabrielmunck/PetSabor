41ac8e2b4478db78a43aeb1d6df342eca257e451
Gabriel A. Munck P. da Cruz <munckgabriel@gmail.com>
2024-10-10 13:15:54 -0300

    Ajustado tamanho da seção Partners para monitores maiores

--- src/components/Partners.tsx
+++ src/components/Partners.tsx
@@ -16,15 +16,15 @@ const Partners: React.FC = () => {
             <section className="bg-forest-green rounded-lg py-12 px-8">
                 <div className="container mx-auto">
                     <h2 className="text-xl sm:text-3xl font-sans font-bold tracking-widest text-center mb-10">Por que escolher a Pet Sabor?</h2>
-                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                         <QualityItem 
                             icon={<FaStar />}
-                            title="Comida de verdade"
+                            title="Comida de Verdade"
                             description="Produtos naturais de alta qualidade para o bem-estar do seu cão"
                         />
                         <QualityItem 
                             icon={<FaLeaf />}
-                            title=" Enriquecimento ambiental"
+                            title=" Enriquecimento Ambiental"
                             description="Enriquecimento ambiental é um conjunto de práticas que melhoram o bem-estar físico e mental dos animais, oferecendo estímulos que promovem comportamentos naturais."
                         />
                         <QualityItem 
